a13c0de12a36b0ae49510aa2266ea81c956982f5
Marc Henry de Frahan (lab) <marchdf@umich.edu>
2016-01-07 12:16:31 -0500

    Minor changes to some scripts (added help functions)

--- bin/pdf2eps.sh
+++ bin/pdf2eps.sh
@@ -1,11 +1,31 @@
 #!/bin/sh
-# $Id: pdf2eps,v 0.01 2005/10/28 00:55:46 Herbert Voss Exp $
-# see http://tex.stackexchange.com/questions/20883/how-to-convert-pdf-to-eps
-# Convert PDF to encapsulated PostScript.
-# usage:
-# pdf2eps <page number> <pdf file without ext>
+#
 
-pdfcrop "$2.pdf" "$2-temp.pdf"
-#pdftops -f $1 -l $1 -eps "$2-temp.pdf" "$2.eps"
-gs -q -dNOCACHE -dNOPAUSE -dBATCH -dSAFER -sDEVICE=epswrite -sOutputFile="$2.eps" "$2-temp.pdf"
-rm  "$2-temp.pdf"
+function myHelp () {
+    cat <<-END
+Usage:
+------
+   -h | --help
+     Convert PDF to encapsulated PostScript (crops the pdf to remove white border)
+     Inspired by http://tex.stackexchange.com/questions/20883/how-to-convert-pdf-to-eps
+     and inspired from pdf2eps, v 0.01 2005/10/28 00:55:46 Herbert Voss Exp
+
+   pdf2eps pdf-file-without-ext
+END
+}
+
+while [ -n "$1" ]; do
+    case "$1" in
+        -h | --help)
+            myHelp
+            exit
+            ;;
+	*)
+	    echo "Converting $1.pdf to an eps file."
+	    pdfcrop "$1.pdf" "$1-temp.pdf"
+	    gs -q -dNOCACHE -dNOPAUSE -dBATCH -dSAFER -sDEVICE=eps2write -sOutputFile="$1.eps" "$1-temp.pdf"
+	    rm  "$1-temp.pdf"
+	    shift
+	    ;;
+    esac 
+done 
--- bin/pdf2epsv2.sh
+++ bin/pdf2epsv2.sh
@@ -1,10 +1,29 @@
 #!/bin/sh
 #
-# Another way to convert pdf to eps. See http://tex.stackexchange.com/questions/20883/how-to-convert-pdf-to-eps
 
-# pdf2epsv2 <pdf file with extension> <outfile with extension>
+function myHelp () {
+    cat <<-END
+Usage:
+------
+   -h | --help
+     Another way to convert PDF to encapsulated PostScript (does not crop the pdf to remove white border)
+     Inspired by http://tex.stackexchange.com/questions/20883/how-to-convert-pdf-to-eps
 
-pdf2ps "$1" "$1-temp.ps";
-ps2eps -f "$1-temp.ps" ;
-mv "$1-temp.eps" "$2";
-rm  -f "$1-temp.ps";
+   pdf2epsv2 <pdf file with extension> <outfile with extension>
+END
+}
+
+case "$1" in
+    -h | --help)
+        myHelp
+        exit
+        ;;
+    *)
+	echo "Converting $1.pdf to an eps file called $2."
+	pdf2ps "$1" "$1-temp.ps";
+	ps2eps -f "$1-temp.ps" ;
+	mv "$1-temp.eps" "$2";
+	rm -f "$1-temp.ps";
+	shift
+	;;
+esac 
