58256ad653962746a268ceed51e44f99bbbd9664
Marc Henry de Frahan (lab) <marchdf@umich.edu>
2016-01-21 10:56:11 -0500

    Minor changes to some scripts and emacs loading.

--- bin/jpg2eps.sh
+++ bin/jpg2eps.sh
@@ -1,7 +1,30 @@
 #!/bin/bash
 #
-# Convert a jpg image to an eps. Takes only one argument: the name of the jpg image
+# 
 #
+function myHelp () {
+    cat <<-END
+Usage:
+------
+   -h | --help
+     Convert a jpg image to an eps. Takes only one argument: the name of the jpg image
 
-file=$1
-convert "$file" eps2:$(echo "$file" | sed 's/\.jpg$/\.eps/')
+   jpg2eps image.jpg
+END
+}
+
+case "$1" in
+    -h | --help)
+        myHelp
+        exit
+        ;;
+    *)
+	echo "Converting $1 to an eps file."
+	file=$1
+	convert "$file" eps2:$(echo "$file" | sed 's/\.jpg$/\.eps/')
+	shift
+	;;
+esac 
+
+
+
