56c9f04c00c4192ddac24175d6a54023fa9c2637
Marc Henry de Frahan (lab) <marchdf@umich.edu>
2016-05-16 14:21:21 -0400

    Added aspell to the install script

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@
 # Setup
 
 # list of files/folders to symlink in homedir
-folders="bash git R screen woof zsh"
+folders="aspell bash git R screen woof zsh"
 
 # list of folders to symlink in homedir (with different src and tgt names)
 declare -A move_folders
