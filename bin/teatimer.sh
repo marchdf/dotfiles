6c174e3ea18f513c2db93803ab74233d5801aa5d
Marc Henry de Frahan (NREL) <marc.henrydefrahan@nrel.gov>
2016-08-18 08:32:59 -0600

    Now the teatimer works for Mac OSX

--- bin/teatimer.sh
+++ bin/teatimer.sh
@@ -1,4 +1,4 @@
-#!/bin/bash    
+#!/usr/bin/env bash
 #
 # Make a tea timer display a notification
 # Input the time, defaults to 3 minutes
@@ -10,29 +10,45 @@ else
     TIME=$1;
 fi
 
+# Wait the specified time
 sleep $TIME;
-notify-send -t 10000 -i /home/marchdf/bin/tea.jpg "Your tea is ready!";
 
-# Another way of doing it, not seconds precision though
-#at now + 5 minutes << EOF 
-#notify-send -t 10000 -i tea.jpg "Your tea is ready!"
-#EOF
 
-# Add a system beep thing. 
+#================================================================================
 #
-# In RHEL6, you first need to modprobe pcspkr you can add it to boot
-# by adding /etc/sysconfig/mymodules.modules and following a
-# combination of
-# https://access.redhat.com/site/documentation/en-US/Red_Hat_Enterprise_Linux/6/html/Deployment_Guide/sec-Persistent_Module_Loading.html
-# and http://www.ndchost.com/wiki/load-modules-on-boot-centos6
+# Notification for Mac platform
 #
-# Also there is something weird in the beep settings so you need to do
-# sudo chmod 4755 /usr/bin/beep (see
-# https://bbs.archlinux.org/viewtopic.php?id=117309 and
-# http://linux.die.net/man/1/beep)
-beep -f 1000 -r 2 -n -r 5 -l 10 --new
+#================================================================================
+if [ "$(uname)" == "Darwin" ]; then
 
+    # Make sure you installed terminal-notifier with homebrew
+    # Also increase the notification banner time by doing:
+    # defaults write com.apple.notificationcenterui bannerTime 15
+    # taken from: https://9to5mac.com/2014/01/30/how-to-change-os-x-banner-notification-duration-using-terminal/
+    terminal-notifier -title "Your tea is ready!" -message "" -sound default -contentImage $HOME/bin/tea.jpg;
 
-# Lots of other beeps at http://www.kirrus.co.uk/2010/09/linux-beep-music/
-# Imperial March
-#beep -l 350 -f 392 -D 100 -n -l 350 -f 392 -D 100 -n -l 350 -f 392 -D 100 -n -l 250 -f 311.1 -D 100 -n -l 25 -f 466.2 -D 100 -n -l 350 -f 392 -D 100 -n -l 250 -f 311.1 -D 100 -n -l 25 -f 466.2 -D 100 -n -l 700 -f 392 -D 100 -n -l 350 -f 587.32 -D 100 -n -l 350 -f 587.32 -D 100 -n -l 350 -f 587.32 -D 100 -n -l 250 -f 622.26 -D 100 -n -l 25 -f 466.2 -D 100 -n -l 350 -f 369.99 -D 100 -n -l 250 -f 311.1 -D 100 -n -l 25 -f 466.2 -D 100 -n -l 700 -f 392 -D 100 -n -l 350 -f 784 -D 100 -n -l 250 -f 392 -D 100 -n -l 25 -f 392 -D 100 -n -l 350 -f 784 -D 100 -n -l 250 -f 739.98 -D 100 -n -l 25 -f 698.46 -D 100 -n -l 25 -f 659.26 -D 100 -n -l 25 -f 622.26 -D 100 -n -l 50 -f 659.26 -D 400 -n -l 25 -f 415.3 -D 200 -n -l 350 -f 554.36 -D 100 -n -l 250 -f 523.25 -D 100 -n -l 25 -f 493.88 -D 100 -n -l 25 -f 466.16 -D 100 -n -l 25 -f 440 -D 100 -n -l 50 -f 466.16 -D 400 -n -l 25 -f 311.13 -D 200 -n -l 350 -f 369.99 -D 100 -n -l 250 -f 311.13 -D 100 -n -l 25 -f 392 -D 100 -n -l 350 -f 466.16 -D 100 -n -l 250 -f 392 -D 100 -n -l 25 -f 466.16 -D 100 -n -l 700 -f 587.32 -D 100 -n -l 350 -f 784 -D 100 -n -l 250 -f 392 -D 100 -n -l 25 -f 392 -D 100 -n -l 350 -f 784 -D 100 -n -l 250 -f 739.98 -D 100 -n -l 25 -f 698.46 -D 100 -n -l 25 -f 659.26 -D 100 -n -l 25 -f 622.26 -D 100 -n -l 50 -f 659.26 -D 400 -n -l 25 -f 415.3 -D 200 -n -l 350 -f 554.36 -D 100 -n -l 250 -f 523.25 -D 100 -n -l 25 -f 493.88 -D 100 -n -l 25 -f 466.16 -D 100 -n -l 25 -f 440 -D 100 -n -l 50 -f 466.16 -D 400 -n -l 25 -f 311.13 -D 200 -n -l 350 -f 392 -D 100 -n -l 250 -f 311.13 -D 100 -n -l 25 -f 466.16 -D 100 -n -l 300 -f 392.00 -D 150 -n -l 250 -f 311.13 -D 100 -n -l 25 -f 466.16 -D 100 -n -l 700 -f 392
+    # It would be nice to get a system beep to work...
+    
+
+#================================================================================
+#
+# Notification for GNU/Linux platform
+#
+#================================================================================
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    notify-send -t 10000 -i $HOME/bin/tea.jpg "Your tea is ready!";
+
+    # Run the system beep thing. 
+    #
+    # In RHEL6, you first need to modprobe pcspkr you can add it to boot
+    # by adding /etc/sysconfig/mymodules.modules and following a
+    # combination of
+    # https://access.redhat.com/site/documentation/en-US/Red_Hat_Enterprise_Linux/6/html/Deployment_Guide/sec-Persistent_Module_Loading.html
+    # and http://www.ndchost.com/wiki/load-modules-on-boot-centos6
+    #
+    # Also there is something weird in the beep settings so you need to do
+    # sudo chmod 4755 /usr/bin/beep (see
+    # https://bbs.archlinux.org/viewtopic.php?id=117309 and
+    # http://linux.die.net/man/1/beep)
+    beep -f 1000 -r 2 -n -r 5 -l 10 --new
+fi
