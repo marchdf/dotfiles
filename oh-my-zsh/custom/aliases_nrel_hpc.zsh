ddffeb9116f80466ff76464447157ae468c536b4
Marc Henry de Frahan (NREL/Peregrine) <marc.henrydefrahan@nrel.gov>
2016-09-01 11:50:28 -0600

    Renamed the peregrine alias file.

--- oh-my-zsh/custom/aliases_nrel_hpc.zsh
+++ oh-my-zsh/custom/aliases_nrel_hpc.zsh
@@ -1,8 +1,8 @@
 #================================================================================
 #
-# These are my custom aliases for my NREL Peregrine machine
+# These are my custom aliases for my NREL HPC machines
 #
 #================================================================================
-if [[ `hostname -A` = *peregrine.hpc.nrel* ]]; then
+if [[ `hostname -f` = *hpc.nrel.gov ]]; then
    alias emacs='/home/mhenryde/builds/emacs-24.5/build/src/emacs'
 fi
