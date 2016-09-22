4e547f7358f0d753e67fa3f7d905d94313b850f8
Marc Henry de Frahan (NREL/Peregrine) <marc.henrydefrahan@nrel.gov>
2016-09-22 08:28:17 -0600

    Added htop alias for hpc nrel

--- oh-my-zsh/custom/aliases_nrel_hpc.zsh
+++ oh-my-zsh/custom/aliases_nrel_hpc.zsh
@@ -4,5 +4,9 @@
 #
 #================================================================================
 if [[ `hostname -f` = *hpc.nrel.gov ]]; then
-   alias emacs='/home/mhenryde/builds/emacs-24.5/build/src/emacs'
+    alias emacs='/home/mhenryde/builds/emacs-24.5/build/src/emacs'
+    alias htop='/home/mhenryde/builds/htop-2.0.2/bin/htop'
+    
+    # Load some custom modules
+    module load python/2.7.8
 fi
