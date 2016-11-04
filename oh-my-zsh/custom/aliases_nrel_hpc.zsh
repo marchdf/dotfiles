fd23fa00e1ab107235e3fd7c55d04a12548a9667
Marc Henry de Frahan (NREL/Peregrine) <marc.henrydefrahan@nrel.gov>
2016-11-04 12:59:11 -0600

    Added some peregrine aliases and a crontab

--- oh-my-zsh/custom/aliases_nrel_hpc.zsh
+++ oh-my-zsh/custom/aliases_nrel_hpc.zsh
@@ -6,7 +6,10 @@
 if [[ `hostname -f` = *hpc.nrel.gov ]]; then
     alias emacs='/home/mhenryde/builds/emacs-24.5/build/src/emacs'
     alias htop='/home/mhenryde/builds/htop-2.0.2/bin/htop'
+
+    alias job_node_ids="qstat -u mhenryde -f | grep -e 'Job\ Id\|exec_host'"
     
     # Load some custom modules
     module load python/2.7.8
+    module use /nopt/nrel/apps/modules/candidate/modulefiles
 fi
