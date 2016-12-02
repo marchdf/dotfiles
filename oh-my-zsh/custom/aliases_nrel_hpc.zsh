f6a4450811b5e85b452935a9c39d25b0e32e7564
Marc Henry de Frahan (NREL/Peregrine) <marc.henrydefrahan@nrel.gov>
2016-12-02 11:45:10 -0700

    Updated paths and crontab for Peregrine

--- oh-my-zsh/custom/aliases_nrel_hpc.zsh
+++ oh-my-zsh/custom/aliases_nrel_hpc.zsh
@@ -7,10 +7,18 @@ if [[ `hostname -f` = *hpc.nrel.gov ]]; then
     alias emacs='/home/mhenryde/builds/emacs-24.5/build/src/emacs'
     alias htop='/home/mhenryde/builds/htop-2.0.2/bin/htop'
 
-    alias job_node_ids="qstat -u mhenryde -f | grep -e 'Job\ Id\|exec_host'"
+    alias job_node_ids="qstat -u mhenryde -f | grep -e 'Job\ Id\|exec_host\|Job_Name'"
     alias cds="cd /scratch/mhenryde"
-    
-    # Load some custom modules
-    module load python/2.7.8
+
+    # Load some custom modules (they need to go here)
+    module purge 
     module use /nopt/nrel/apps/modules/candidate/modulefiles
+    module load openmpi-gcc/1.10.0-5.2.0
+    module load gcc/5.2.0 
+    module load python/2.7.8
+
+    # Spack (according to Jon Rood)
+    export SPACK_ROOT=${HOME}/spack
+    source $SPACK_ROOT/share/spack/setup-env.sh
+
 fi
