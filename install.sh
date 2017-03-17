eed5d973579c4cae2f18c0b9339a16c8316929dd
Marc Henry de Frahan <marc.henrydefrahan@nrel.gov>
2017-03-17 08:40:03 -0600

    Move dotfiles install to use GNU stow.

--- install.sh
+++ install.sh
@@ -1,101 +1,52 @@
 #!/usr/bin/env bash
-#================================================================================
-#
-# Create symlinks of dotfiles
-#
-#================================================================================
 
 #================================================================================
-# Setup
+# Functions
 
-# list of files/folders to symlink in homedir
-folders="aspell bash git R screen woof zsh sleepwatcher amrvis"
+# Create symlinks of dotfiles using stow
+install_dotfiles() {
+    stow amrvis
+    stow aspell
+    stow bash
+    stow bin
+    stow crontab_files
+    stow emacs
+    stow git
+    stow mypython
+    stow R
+    stow rsync_excludes
+    stow screen
+    stow sleepwatcher
+    stow woof
+    stow zsh
+}
 
-# list of folders to symlink in homedir (with different src and tgt names)
-declare -A move_folders
-move_folders[bin]=bin
-move_folders[mypython]=mypython
-move_folders[rsync_excludes]=rsync_excludes
-move_folders[crontab_files]=crontab_files
-
-#================================================================================
-# Configure directories
-DOTDIR=~/dotfiles       # dotfiles directory
-OLDDIR=~/dotfiles_old   # old dotfiles backup directory
-
-# create dotfiles_old in homedir
-echo "Creating a clean $OLDDIR for backup of any existing dotfiles in ~"
-mv $OLDDIR $OLDDIR.bkp
-mkdir -p $OLDDIR
-echo "...done"
-
-# change to the dotfiles directory
-echo "Changing to the $DOTDIR directory"
-cd $DOTDIR
-echo "...done"
-
-#================================================================================
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
-for folder in $folders; do
-
-    # get all the files in the directory
-    files=$DOTDIR/$folder/*
-
-    # loop on the files
-    for file in $files; do
-
-	# remove leading path name up to the last /
-	fname=${file##*/} 
-
-	# backup old copy of the file
-    	echo "Moving .$fname from ~ to $OLDDIR"
-	mv ~/.$fname $OLDDIR/
-
-	# create symlink
-    	echo "Creating symlink to .$fname in home directory."
-	ln -s $file ~/.$fname
-    done
-done
+# Remove symlinks of dotfiles using stow
+uninstall_dotfiles() {
+    stow -D amrvis
+    stow -D aspell
+    stow -D bash
+    stow -D bin
+    stow -D crontab_files
+    stow -D emacs
+    stow -D git
+    stow -D mypython
+    stow -D R
+    stow -D rsync_excludes
+    stow -D screen
+    stow -D sleepwatcher
+    stow -D woof
+    stow -D zsh
+}
 
 #================================================================================
-# We have to be a bit careful with emacs
-echo "Moving .emacs from ~ to $OLDDIR"
-mv ~/.emacs $OLDDIR
-echo "Creating symlink to .emacs in home directory."
-ln -s $DOTDIR/emacs/emacs ~/.emacs
-echo "Making sure .emacs.d exists"
-mkdir -p .emacs.d
-echo "Creating symlink to personal in .emacs.d directory."
-ln -s $DOTDIR/emacs/emacs.d/personal ~/.emacs.d/
+# Main
 
+# Make sure GNU stow is installed
+if ! command -v stow >/dev/null 2>&1; then
+    echo "Please install GNU stow"
+    exit 1
+fi    
 
-#================================================================================
-# These aren't really dotfiles but I want them version
-# controlled. Copy them straight to my home without the dot stuff
-for folder in "${!move_folders[@]}"; do
-    src=$folder
-    echo $src
-    tgt=${move_folders[$folder]}
-
-    # backup old copy of the file
-    echo "Moving $tgt from ~ to $OLDDIR"
-    mv ~/$tgt $OLDDIR/
-
-    # create symlink
-    echo "Creating symlink from $DOTDIR/$src to ~/$tgt."
-    ln -s $DOTDIR/$src ~/$tgt
-    
-done
-
-#================================================================================
-# Oh-my-zsh custom files
-echo "Creating symlinks for the oh-my-zsh custom files"
-tgtdir=~/.oh-my-zsh/custom
-mkdir -p $tgtdir
-for file in $DOTDIR/oh-my-zsh/custom/*; do
-    ln -s $file $tgtdir/$(basename $file)
-done
-
-#================================================================================
-# Clean backup dotfiles directory
-rm -rf $OLDDIR.bkp
+# install (or uninstall)
+install_dotfiles
