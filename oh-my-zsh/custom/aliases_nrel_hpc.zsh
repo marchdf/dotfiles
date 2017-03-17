564bad329a1a08cba123e2c254330896c4eda30b
Marc Henry de Frahan (NREL/Peregrine) <marc.henrydefrahan@nrel.gov>
2017-03-17 06:52:46 -0600

    Moved module load into zshenv

--- oh-my-zsh/custom/aliases_nrel_hpc.zsh
+++ oh-my-zsh/custom/aliases_nrel_hpc.zsh
@@ -8,25 +8,4 @@ if [[ `hostname -f` = *hpc.nrel.gov ]]; then
     alias job_node_ids="qstat -u mhenryde -f | grep -e 'Job\ Id\|exec_host\|Job_Name'"
     alias cds="cd /scratch/mhenryde"
 
-    # Load some custom modules (they need to go here)
-    {
-	module purge 
-	module use /nopt/nrel/apps/modules/candidate/modulefiles
-	module load openmpi-gcc/1.10.0-5.2.0
-	module load gcc/5.2.0 
-	module load python/2.7.8
-	module load R
-    } &> /dev/null
-
-    {
-	. ${HOME}/.oh-my-zsh/custom/functions.zsh
-	load_spack
-	spack load binutils
-	spack load texlive
-	spack load screen
-	spack load git
-	spack load emacs
-	spack load global
-    } &> /dev/null
-
 fi
