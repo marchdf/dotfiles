4677d3de1ab07ebeed8f5a9bbe90dacb32032ffe
Marc Henry de Frahan <marchdf@gmail.com>
2023-01-18 14:20:43 -0700

    Update nrel hpc functions

--- zsh/.oh-my-zsh/custom/functions_nrel_hpc.zsh
+++ zsh/.oh-my-zsh/custom/functions_nrel_hpc.zsh
@@ -12,6 +12,7 @@ if [ "${NREL_CLUSTER}" = "eagle" ] || [ "${NREL_CLUSTER}" = "rhodes" ]; then
             module load gcc
             module load mpt
             module load binutils
+            module load bzip2
             module load cmake
             module load emacs
             module load ffmpeg/4.2.2
@@ -19,9 +20,12 @@ if [ "${NREL_CLUSTER}" = "eagle" ] || [ "${NREL_CLUSTER}" = "rhodes" ]; then
             module load gnutls
             module load htop
             module load image-magick
+            module load libffi
             module load libtool
+            module load openssl
             module load python/3.7.7
             module load py-setuptools-scm
+            module load sqlite
             module load texlive
             module load tmux
         } &> /dev/null
@@ -54,6 +58,11 @@ if [ "${NREL_CLUSTER}" = "eagle" ] || [ "${NREL_CLUSTER}" = "rhodes" ]; then
         vglrun /nopt/nrel/ecom/exawind/exawind/install/paraview/5.9.0/bin/paraview
     }
 
+    function pv510gui() {
+        module load paraview/5.10.1-gui
+        vglrun paraview
+    }
+
     function pv58server() {
         source /nopt/nrel/ecom/exawind/exawind/scripts/exawind-env-gcc.sh
         module load paraview/5.8.1-server
@@ -75,6 +84,25 @@ if [ "${NREL_CLUSTER}" = "eagle" ] || [ "${NREL_CLUSTER}" = "rhodes" ]; then
 
     function pvtunnel() {
         ssh -L 11111:$1:11111 mhenryde@$1
+    }
+
+    function pyenv_python_install(){
+        module load gcc
+        module load mpt
+        module load binutils
+        module load git
+        module load gnutls
+        module load libtool
+        module load texlive
+        module load bzip2
+        module load libffi
+        module load openssl
+        module load sqlite
+        export LDFLAGS="-L${OPENSSL_ROOT_DIR}/lib -L${LIBFFI_ROOT_DIR}/lib64 -L${BZIP2_ROOT_DIR}/lib -L${SQLITE_ROOT_DIR}/lib"
+        export CPPFLAGS="-I${OPENSSL_ROOT_DIR}/include -I${LIBFFI_ROOT_DIR}/include -I${BZIP2_ROOT_DIR}/include -I${SQLITE_ROOT_DIR}/include"
+        export PYTHON_CONFIGURE_OPTS="--enable-optimizations --with-lto"
+        export PYTHON_CFLAGS="-mtune=native"
+        pyenv install
     }
 
 fi
