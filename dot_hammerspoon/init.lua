78c18569f2be764dfa9d271fc0c2bd5d2e6637b1
Marc Henry de Frahan <marchdf@gmail.com>
2023-10-13 10:44:20 -0600

    Comment out a bunch of hammerspoon and minor emacs update

--- dot_hammerspoon/init.lua
+++ dot_hammerspoon/init.lua
@@ -1,48 +1,48 @@
 -- Enable some PC-like commands, but disable in some apps
-local hk_copy = hs.hotkey.bind({"ctrl"}, "c", nil, function() hs.eventtap.keyStroke({"cmd"}, "c") end)
-local hk_cut = hs.hotkey.bind({"ctrl"}, "x", nil, function() hs.eventtap.keyStroke({"cmd"}, "x") end)
-local hk_paste = hs.hotkey.bind({"ctrl"}, "v", nil, function() hs.eventtap.keyStroke({"cmd"}, "v") end)
-local hk_save = hs.hotkey.bind({"ctrl"}, "s", nil, function() hs.eventtap.keyStroke({"cmd"}, "s") end)
-local hk_undo = hs.hotkey.bind({"ctrl"}, "z", nil, function() hs.eventtap.keyStroke({"cmd"}, "z") end)
-local hk_find = hs.hotkey.bind({"ctrl"}, "f", nil, function() hs.eventtap.keyStroke({"cmd"}, "f") end)
-local hk_copy_uname = hs.hotkey.bind({"ctrl"}, "b", nil, function() hs.eventtap.keyStroke({"cmd"}, "b") end)
+-- local hk_copy = hs.hotkey.bind({"ctrl"}, "c", nil, function() hs.eventtap.keyStroke({"cmd"}, "c") end)
+-- local hk_cut = hs.hotkey.bind({"ctrl"}, "x", nil, function() hs.eventtap.keyStroke({"cmd"}, "x") end)
+-- local hk_paste = hs.hotkey.bind({"ctrl"}, "v", nil, function() hs.eventtap.keyStroke({"cmd"}, "v") end)
+-- local hk_save = hs.hotkey.bind({"ctrl"}, "s", nil, function() hs.eventtap.keyStroke({"cmd"}, "s") end)
+-- local hk_undo = hs.hotkey.bind({"ctrl"}, "z", nil, function() hs.eventtap.keyStroke({"cmd"}, "z") end)
+-- local hk_find = hs.hotkey.bind({"ctrl"}, "f", nil, function() hs.eventtap.keyStroke({"cmd"}, "f") end)
+-- local hk_copy_uname = hs.hotkey.bind({"ctrl"}, "b", nil, function() hs.eventtap.keyStroke({"cmd"}, "b") end)
 
-local function hk_enable()
-  hk_copy:enable()
-  hk_cut:enable()
-  hk_paste:enable()
-  hk_save:enable()
-  hk_undo:enable()
-  hk_find:enable()
-end
+-- local function hk_enable()
+--   hk_copy:enable()
+--   hk_cut:enable()
+--   hk_paste:enable()
+--   hk_save:enable()
+--   hk_undo:enable()
+--   hk_find:enable()
+-- end
 
-local function hk_disable()
-  hk_copy:disable()
-  hk_cut:disable()
-  hk_paste:disable()
-  hk_save:disable()
-  hk_undo:disable()
-  hk_find:disable()
-end
+-- local function hk_disable()
+--   hk_copy:disable()
+--   hk_cut:disable()
+--   hk_paste:disable()
+--   hk_save:disable()
+--   hk_undo:disable()
+--   hk_find:disable()
+-- end
 
-local wf=hs.window.filter
-wf_terminal = wf.new{'Terminal', 'iTerm2', 'Emacs'}
-wf_iterm2 = wf.new(false):setAppFilter('iTerm2',{allowTitles=1})
-wf_keepass = wf.new(false):setAppFilter('KeePassXC',{allowTitles=1})
+-- local wf=hs.window.filter
+-- wf_terminal = wf.new{'Terminal', 'iTerm2', 'Emacs'}
+-- wf_iterm2 = wf.new(false):setAppFilter('iTerm2',{allowTitles=1})
+-- wf_keepass = wf.new(false):setAppFilter('KeePassXC',{allowTitles=1})
 
-wf_terminal
-    :subscribe(hs.window.filter.windowFocused, hk_disable)
-    :subscribe(hs.window.filter.windowUnfocused, hk_enable)
+-- wf_terminal
+--     :subscribe(hs.window.filter.windowFocused, hk_disable)
+--     :subscribe(hs.window.filter.windowUnfocused, hk_enable)
 
-wf_iterm2
-    :subscribe(hs.window.filter.windowFocused, hk_disable)
-    :subscribe(hs.window.filter.windowUnfocused, hk_enable)
+-- wf_iterm2
+--     :subscribe(hs.window.filter.windowFocused, hk_disable)
+--     :subscribe(hs.window.filter.windowUnfocused, hk_enable)
 
-wf_keepass
-    :subscribe(hs.window.filter.windowFocused, function() hk_copy_uname:enable() end)
-    :subscribe(hs.window.filter.windowUnfocused, function() hk_copy_uname:disable() end)
+-- wf_keepass
+--     :subscribe(hs.window.filter.windowFocused, function() hk_copy_uname:enable() end)
+--     :subscribe(hs.window.filter.windowUnfocused, function() hk_copy_uname:disable() end)
 
-hk_enable()
+-- hk_enable()
 
 -- Use side buttons on mouse to scroll
 local back    = 3
