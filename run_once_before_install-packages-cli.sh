28d4e3c38edd4666bcbfef6cde10244ae1959226
Marc Henry de Frahan <marc.henrydefrahan@nrel.gov>
2024-01-25 16:20:04 -0700

    remove z.lua replace with zoxide

--- run_once_before_install-packages-cli.sh
+++ run_once_before_install-packages-cli.sh
@@ -3,3 +3,8 @@
 if [[ ! -x "$(command -v poetry)" ]]; then
     curl -sSL https://install.python-poetry.org | POETRY_VERSION=1.7.0 POETRY_HOME=${HOME}/.poetry python3 -
 fi
+
+if [[ ! -x "$(command -v zoxide)" ]]; then
+    curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
+fi
+
