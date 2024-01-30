7ba7975af036ae7ea0a220a4f89a83f90bfd7660
Marc Henry de Frahan <marc.henrydefrahan@nrel.gov>
2024-01-29 20:33:50 -0700

    install zsh and ohmyzsh

--- run_once_before_install-packages-cli.sh
+++ run_once_before_install-packages-cli.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+if [ ! -d "${HOME}/.oh-my-zsh" ]; then
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
 if [[ ! -x "$(command -v poetry)" ]]; then
     curl -sSL https://install.python-poetry.org | POETRY_VERSION=1.7.0 POETRY_HOME=${HOME}/.poetry python3 -
 fi
