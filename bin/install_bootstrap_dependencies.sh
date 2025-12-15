4c7e94b1791621dc1254839c4823c90d4c30ccbd
Cassie <marchdf@gmail.com>
2025-12-15 08:32:04 -0700

    test

---
+++ bin/install_bootstrap_dependencies.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+OS="$(uname -s)"
+
+if [[ "$OS" == "Darwin" ]]; then
+    brew install zsh chezmoi zoxide poetry
+elif [[ "$OS" == "Linux" ]]; then
+    if [[ -f /etc/os-release ]]; then
+        . /etc/os-release
+        if [[ "$ID" == "ubuntu" ]]; then
+	    sudo apt-get update
+	    sudo apt install -y \
+		 git \
+		 zsh \
+		 curl \
+		 zoxide \
+		 python3-poetry \
+		 make \
+		 wget \
+		 llvm \
+		 build-essential \
+		 libbz2-dev \
+		 libreadline-dev \
+		 liblzma-dev \
+		 libsqlite3-dev \
+		 libssl-dev \
+		 zlib1g-dev \
+		 libncurses5-dev \
+		 libncursesw5-dev \
+		 libgdbm-dev \
+		 libnss3-dev \
+		 xz-utils \
+		 tk-dev \
+		 libffi-dev \
+		 uuid-dev
+        else
+            echo "Linux (Not Ubuntu: $NAME)"
+        fi
+    else
+        echo "Linux (Unknown distro)"
+    fi
+    sh -c "$(curl -fsLS get.chezmoi.io)" -- -b $HOME/.local/bin
+    export PATH="${HOME}/.local/bin:$PATH"
+else
+    echo "Unknown OS: $OS"
+fi
